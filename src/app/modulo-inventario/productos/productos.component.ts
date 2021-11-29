26256ef6d4a99e2c20621c89f1973770da7df470
merlingja <merling.aguilar@unah.hn>
2021-11-28 21:46:53 -0600

    se agrego modal para actualizar productos

--- src/app/modulo-inventario/productos/productos.component.ts
+++ src/app/modulo-inventario/productos/productos.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ServiceInventario} from '../../SERVICES/services_inventario/moduloInventario.service';
-import {Router} from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
+import {FormGroup, FormBuilder} from '@angular/forms';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-productos',
@@ -11,22 +14,77 @@ import {Router} from '@angular/router';
 
 export class ProductosComponent implements OnInit {
     products:any = [];
+    updateForm: FormGroup;
+    titulo: string ='';
 
- constructor(private serviceInventario:ServiceInventario, private router:Router) { }
+ constructor(
+  private serviceInventario:ServiceInventario, 
+  private router:Router,
+  private activatedRoute: ActivatedRoute,
+  public formBuilder: FormBuilder ) {
+
+    this.updateForm = this.formBuilder.group({
+      cod_producto: '',
+      cod_tip_producto:'',
+      nombre_producto:'',
+      precio:'', 
+      fotografia:'', 
+      vida_util:'',
+      descripcion:'',
+      pre_producto:'',
+      fec_caducidad:''
+
+      });
+   }
 
 
   ngOnInit(): void {
+    this.MostrarDatos();
+    
+  }
+
+  MostrarDatos(){
     this.serviceInventario.GetProductos().subscribe(res => {
       console.log(res)
       this.products =<any>res;
     });
-    
+
   }
 
-  modalEditar(id:number){
-    console.log(id)
-    return this.router.navigate(['/panel',{outlets:{outlet_modal_editar:['editar-producto',id]}}]);
+  EditarProducto(id:number){
+    this.serviceInventario.GetProducto(id).subscribe(res => {
+
+      console.log(res)
+      this.updateForm.setValue({
+      cod_producto: id,
+      cod_tip_producto: res['COD_TIP_PRODUCTO'],
+      nombre_producto: res ['NOMBRE_PRODUCTO'],
+      precio: res ['PRECIO'], 
+      fotografia: res ['FOTOGRAFIA'],  
+      vida_util: res ['VIDA_UTIL'], 
+      descripcion: res ['DESCRIPCION'], 
+      pre_producto: res ['PRE_PRODUCTO'], 
+      fec_caducidad: res ['FEC_CADUCIDAD']
+
+      });
+    });
   }
+
+  onUpdate(): any {
+    this.serviceInventario.updateProductos(this.updateForm.value)
+    .subscribe(() => {
+        console.log('Data updated successfully!')
+        Swal.fire('Se actualizo con exito',this.titulo,'success')
+        this.MostrarDatos()
+        
+      
+      }, (err) => {
+        console.log(err);
+        Swal.fire('Ocurrio problema',this.titulo,'error')
+    });
+  }
+
+   
 
 }
 
