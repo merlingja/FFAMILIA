fd290970d71ba78a941b6e17434efcec687ce57d
Deiby-Lainez <90879436+Deiby-Lainez@users.noreply.github.com>
2021-12-04 19:32:53 -0600

    Modificacion a modulo AppQR

--- src/app/modulo-Appqr/CodigoQr/codigoqr.component.ts
+++ src/app/modulo-Appqr/CodigoQr/codigoqr.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ServiceAppQr} from '../../SERVICES/services-appqr/moduloappqr.service';
-import {Router} from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
+import {FormGroup, FormBuilder} from '@angular/forms';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-codigoqr',
@@ -9,18 +12,107 @@ import {Router} from '@angular/router';
 })
 export class CodigoQrComponent implements OnInit {
   Codigos:any = [];
+  updateForm: FormGroup;
+  createForm: FormGroup;
+  titulo: string ='';
+
+   constructor(
+     private serviceAppQr:ServiceAppQr, 
+     private router:Router,
+     private activatedRoute: ActivatedRoute,
+     public formBuilder: FormBuilder) {
+
+      this.updateForm = this.formBuilder.group({
+        cod_qr: '',
+        seq_qr: '',
+        usr_registro:''
+       
   
-   constructor(private serviceAppQr:ServiceAppQr, private router:Router) { }
+        });
+
+        this.createForm = this.formBuilder.group({
+        sequencia: '',
+        usuario: ''
+        
+        });
+  
+
+      }
+
+   
 
 
-  ngOnInit(): void {
-    this.serviceAppQr.GetCodigos().subscribe(res => {
-      console.log(res)
-      this.Codigos =<any>res;
-    });
+      ngOnInit(): void {
+        this.ListarCodigos();
+          }
     
-  }
-
+      ListarCodigos(){
+        this.serviceAppQr.GetCodigoss().subscribe(res => {
+          console.log(res)
+          this.Codigos =<any>res;
+        });
+    
+      }
+      
+      EditarCodigos(id:number){
+        this.serviceAppQr.GetCodigoQR(id).subscribe(res => {
+    
+          console.log(res)
+          this.updateForm.setValue({
+            cod_qr: id,
+            seq_qr: res['SEQ_QR'],
+            usr_registro: res['USR_REGISTRO']
+           
+    
+          });
+        });
+      }
+    
+      onUpdate(): any {
+        this.serviceAppQr.updateCodigoss(this.updateForm.value)
+        .subscribe(() => {
+            console.log('Data updated successfully!')
+            Swal.fire('Se actualizo con exito',this.titulo,'success')
+            this.ListarCodigos()
+            
+          
+          }, (err) => {
+            console.log(err);
+            Swal.fire('Ocurrio problema',this.titulo,'error')
+        });
+      }
   
-
+      eliminarCodigos(id:any, i:number):any{
+        if(window.confirm('Esta seguro de querer elimina el registro?')){
+          this.serviceAppQr.borrarCodigoss(id).subscribe(res=>{
+            if(res.affectedRows==1){
+              console.log("Se elimino el registro");
+              Swal.fire('Se elimino con exito',this.titulo,'success')
+    
+              this.Codigos.splice(i, 1);
+            }else{
+              console.log("No se pudo eliminar el registro o, no existe");
+              window.alert("error")
+              
+            }
+          }, (err) => {
+            console.log(err);
+            Swal.fire('Ocurrio problema',this.titulo,'error')
+        }
+        
+        );
+        }
+      }
+      
+      nuevoCodigo(): any {
+        this.serviceAppQr.crearCodigoss( this.createForm.value).subscribe(() => {
+            console.log('Data updated successfully!')
+            Swal.fire('Se Inserto con exito',this.titulo,'success')
+             this.ListarCodigos();
+             
+          }, (err) => {
+            console.log(err);
+            Swal.fire('Ocurrio problema',this.titulo,'error')
+        });
+      }
 }
--- src/app/modulo-Appqr/Plantas/plantas.component.ts
+++ src/app/modulo-Appqr/Plantas/plantas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ServiceAppQr} from '../../SERVICES/services-appqr/moduloappqr.service';
-import {Router} from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
+import {FormGroup, FormBuilder} from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-plantas',
@@ -9,18 +11,115 @@ import {Router} from '@angular/router';
 })
 export class PlantasComponent implements OnInit {
   Plantas:any = [];
+  updateForm: FormGroup;
+  createForm: FormGroup;
+  titulo: string ='';
+
+   constructor(
+     private serviceAppQr:ServiceAppQr, 
+     private router:Router,
+     private activatedRoute: ActivatedRoute,
+     public formBuilder: FormBuilder) {
+
+      this.updateForm = this.formBuilder.group({
+        cod_planta: '',
+        nom_planta: '',
+        nom_cientifico:'',
+        clase:'',
+        familia:'',
+        especie:'',
+        des_planta: ''
   
-   constructor(private serviceAppQr:ServiceAppQr, private router:Router) { }
+        });
+
+        this.createForm = this.formBuilder.group({
+        nombre: '',
+        nombre_cientifico: '',
+        clases: '',
+        familias: '',
+        especies: '',
+        descripcion:''
+        });
+  
+
+      }
 
 
-  ngOnInit(): void {
-    this.serviceAppQr.GetPlantass().subscribe(res => {
-      console.log(res)
-      this.Plantas =<any>res;
-    });
+      ngOnInit(): void {
+        this.ListarPlantas();
+          }
     
-  }
+      ListarPlantas(){
+        this.serviceAppQr.GetPlantass().subscribe(res => {
+          console.log(res)
+          this.Plantas =<any>res;
+        });
+    
+      }
 
-  
+      EditarPlantas(id:number){
+        this.serviceAppQr.GetPlanta(id).subscribe(res => {
+    
+          console.log(res)
+          this.updateForm.setValue({
+            cod_planta: id,
+            nom_planta: res['NOM_PLANTA'],
+            nom_cientifico: res['NOM_CIENTIFICO'],
+            clase: res['CLASE'],
+            familia: res['FAMILIA'],
+            especie: res['ESPECIE'],
+            des_planta: res['DES_PLANTA']
+    
+    
+          });
+        });
+      }
+    
+      onUpdate(): any {
+        this.serviceAppQr.updatePlantass(this.updateForm.value)
+        .subscribe(() => {
+            console.log('Data updated successfully!')
+            Swal.fire('Se actualizo con exito',this.titulo,'success')
+            this.ListarPlantas()
+            
+          
+          }, (err) => {
+            console.log(err);
+            Swal.fire('Ocurrio problema',this.titulo,'error')
+        });
+      }
+      
+      eliminarPlantas(id:any, i:number):any{
+        if(window.confirm('Esta seguro de querer elimina el registro?')){
+          this.serviceAppQr.borrarPlantass(id).subscribe(res=>{
+            if(res.affectedRows==1){
+              console.log("Se elimino el registro");
+              Swal.fire('Se elimino con exito',this.titulo,'success')
+    
+              this.Plantas.splice(i, 1);
+            }else{
+              console.log("No se pudo eliminar el registro o, no existe");
+              window.alert("error")
+              
+            }
+          }, (err) => {
+            console.log(err);
+            Swal.fire('Ocurrio problema',this.titulo,'error')
+        }
+        
+        );
+        }
+      }
 
+      nuevaPlanta(): any {
+        this.serviceAppQr.crearPlantass( this.createForm.value).subscribe(() => {
+            console.log('Data updated successfully!')
+            Swal.fire('Se Inserto con exito',this.titulo,'success')
+             this.ListarPlantas();
+             
+          }, (err) => {
+            console.log(err);
+            Swal.fire('Ocurrio problema',this.titulo,'error')
+        });
+      }
 }
